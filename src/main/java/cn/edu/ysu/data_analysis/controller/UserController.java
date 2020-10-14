d918782dcefc70764fa6c3cd8e04a19915d9f9aa
jiajun169 <shenjiajun169@gmail.com>
2020-10-14 15:41:17 +0800

    小修改

--- src/main/java/cn/edu/ysu/data_analysis/controller/UserController.java
+++ src/main/java/cn/edu/ysu/data_analysis/controller/UserController.java
@@ -6,8 +6,12 @@ import cn.edu.ysu.data_analysis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/user")
@@ -30,5 +34,11 @@ public class UserController {
     public Result update(User user, HttpSession session) {
         session.setAttribute("user", user);
         return userService.update(user);
+    }
+
+    @RequestMapping("/logout")
+    public void logout(HttpServletResponse response, HttpSession session) throws IOException {
+        session.removeAttribute("user");
+        response.sendRedirect("/login.html");
     }
 }
